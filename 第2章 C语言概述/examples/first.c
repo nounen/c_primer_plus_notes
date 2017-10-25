e692723c62955c3a24018888e87cac5742a60dd2
nounen <281580047@qq.com>
2017-10-25 22:39:06 +0800

    2.1 简单的C程序示例

---
+++ 第2章 C语言概述/examples/first.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+
+int main(void)
+{
+    int num;
+    num = 1;
+
+    printf("I am a simple");
+    printf("computer.\n");
+    printf("My　favorite　number　is　%d　because　it　is　first.\n", num);
+
+    // getchar(); // 加一行解决 程序的输出在屏幕上一闪而过 的问题 （linux 环境表示没有这个问题
+
+    return 0;
+}
